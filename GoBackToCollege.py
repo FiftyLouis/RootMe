9dc23375d68ec07b68721c2b37cd0206e23759e5
FiftyLouis <louis17b@gmail.com>
2022-05-16 11:00:26 +0200

    bug fix bytes irc

--- GoBackToCollege.py
+++ GoBackToCollege.py
@@ -3,40 +3,40 @@ import socket, time, os, math
 
 def pingpong():
     while 1:
-        junk=b""
+        junk=""
         junk = irc.recv(7000)
         print(junk)
-        beg = junk.find(b"PING")
+        beg = junk.find("PING")
         print(beg)
         if beg>-1:
             junk=junk[:beg]
-            if junk[5:].find(b" ")>0:
-                junk=junk[:(junk[5:].find(b" "))+4]+b"\r\n"
+            if junk[5:].find(" ")>0:
+                junk=junk[:(junk[5:].find(" "))+4]+b"\r\n"
 
-            junk = junk.replace(b"PING",b"PONG")
+            junk = junk.replace("PING","PONG")
             irc.send(junk)
             irc.send(junk)
             break
 
 
 def main():
-    irc.send(b"PRIVMS "+ bot + b" :!ep1\r\n")
+    irc.send(b"PRIVMS candy :!ep1\r\n")
     while 1:
         junk = b""
         junk = irc.recv(7000)
         print(junk)
-        if junk.find(b"/")>-1:
+        if junk.find("/")>-1:
             try:
                 junk = junk.split(":")
                 junk = junk[1]
                 print(junk)
-                nb1 = int(junk.split(b"/")[0])
-                nb2 = int(junk.split(b"/")[1])
+                nb1 = int(junk.split("/")[0])
+                nb2 = int(junk.split("/")[1])
                 result = round(math.sqrt(nb1)*nb2, 2)
                 result = bytes(str(result)).decode("ASCII")
-                irc.send(b"PRIVMSG "+bot+b" :!ep1 -rep "+ result+b"\r\n")
+                irc.send("PRIVMSG "+bot+b" :!ep1 -rep "+ result+"\r\n")
                 print(irc.recv(7000))
-                irc.send(b"QUIT :By3 By3!")
+                irc.send("QUIT :By3 By3!")
                 break
             except:
                 print("waiting for challenge") 
@@ -56,13 +56,13 @@ except:
     print("[!] Can't connect!")
 else:
     print("[+] Sending nickname")
-    irc.send(b"NICK FiftyLouis\r\n")
+    irc.send("NICK FiftyLouis\r\n")
     print("[+] Sending USER command")
-    irc.send(b"USER FiftyLouis irc.root-me.org root-me :ChallengeBot")
+    irc.send("USER FiftyLouis irc.root-me.org root-me :ChallengeBot")
     print("[+] Join",channel)
-    irc.send(b"JOIN" + channel)
-    print("[+] Playing PING PONG to impose our bot presence")
-    pingpong()
+    irc.send("JOIN" + channel)
+    #print("[+] Playing PING PONG to impose our bot presence")
+    #pingpong()
     print("execute main")
     main()
 
